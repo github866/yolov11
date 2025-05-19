e64b50172f25ec7fd1be894d68efb11d3ac09468
zhaoheng_zhu <zhaoheng_zhu@csce-zhang-s1.engr.tamu.edu>
2025-05-19 04:12:08 -0500

    maybe a necessary update on sh file

--- run_track.sh
+++ run_track.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 CUDA_VISIBLE_DEVICES=0 python leo_run_track.py \
-    --video_path /data/leohsu/human_dataset/humans/clips_02/clip_01.mp4 \
+    --video_path /data/zhaoheng_zhu/origin/output_by_parts/loc02/part_02.mp4 \
     --output_path ./results/clips_02_01.mp4 \
-    --metadata_path /data/leohsu/human_dataset/humans/metadata/init_frames/loc02-frame0001.data \
+    --metadata_path /data/zhaoheng_zhu/human_data/filter_data/loc02-frame0001.data
