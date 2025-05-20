3ca6af340799772d1bf7123ff3c224b0c2a27361
zhaoheng_zhu <zhaoheng_zhu@csce-zhang-s1.engr.tamu.edu>
2025-05-20 07:53:04 -0500

    use final clip

--- run_track.sh
+++ run_track.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 python leo_run_track.py \
-    --video_path /data/leohsu/human_dataset/humans/clips_02/clip_01.mp4 \
-    --output_path ./results/clips_02_01.mp4 \
-    --metadata_path /data/leohsu/human_dataset/humans/metadata/init_frames/loc02-frame0001.data
+    --video_path /data/leohsu/human_dataset/humans/clips_02/clip_06.mp4 \
+    --output_path ./results/clips_02_06.mp4 \
+    --metadata_path /data/leohsu/human_dataset/humans/metadata/init_frames/loc02-frame4501.data
